74e2fdc8b10e8b114e6c53ff70787c261e0a68c5
Igor <igoraraujobda@gmail.com>
2023-01-11 15:52:09 -0300

    implementação das funções de fetch para catergories e categorias+query

--- src/services/api.js
+++ src/services/api.js
@@ -1,12 +1,22 @@
 export async function getCategories() {
   // Implemente aqui
+  const endPoint = 'https://api.mercadolibre.com/sites/MLB/categories';
+
+  const response = await fetch(endPoint);
+  const data = response.json();
+  return data;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
   // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+  const endPoint = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`;
+  const response = await fetch(endPoint);
+  const data = response.json();
+  return data;
 }
 
 export async function getProductById() {
   // Esta implementação específica não é avaliada, mas pode ajudar você 🙂
   // Atenção: essa função não deverá ser chamada na tela do carrinho de compras.
+
 }
