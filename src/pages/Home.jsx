9a703e3dadd17205b254c14c90537bc9968b2682
Igor <igoraraujobda@gmail.com>
2023-01-11 20:19:30 -0300

    req5-listarProdutosPorTermo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,26 +1,65 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import Categories from '../component/Categories';
+import { getProductsFromCategoryAndQuery } from '../services/api';
 
 export default class Home extends Component {
   state = {
     isInputEmpty: true,
+    itemSearch: '',
+    itemConteiner: [],
+    /*     categoryId: '',
+    query: "", */
+  };
+
+  /*   componentDidMount() {
+    this.filterItems();
+  } */
+
+  /*   validateSearchParams = () => {
+    const { itemSearch } = this.state;
+
+    if (itemSearch.includes('MLB')) {
+      this.setState({
+        categoryId: itemSearch,
+        query: "",
+      }, this.filterItems);
+    } else {
+      this.setState({
+        categoryId: "",
+        query: itemSearch,
+      }, this.filterItems);
+    }
+  }; */
+
+  filterItems = async () => {
+    // const { categoryId, query } = this.setState;
+    const { itemSearch, itemConteiner } = this.state;
+    const products = await getProductsFromCategoryAndQuery('', itemSearch);
+
+    this.setState({
+      itemConteiner: products.results,
+    });
+
+    console.log(itemConteiner);
   };
 
   handleChange = ({ target }) => (
     target.value.length > 0 ? (
       this.setState({
         isInputEmpty: false,
-      })
+        itemSearch: target.value,
+      }, this.validateSearchParams)
     ) : (
       this.setState({
         isInputEmpty: true,
-      })
+        itemSearch: target.value,
+      }, this.validateSearchParams)
     )
   );
 
   render() {
-    const { isInputEmpty } = this.state;
+    const { isInputEmpty, itemConteiner } = this.state;
     return (
       <div>
         <form>
@@ -30,9 +69,17 @@ export default class Home extends Component {
               name="itemSearch"
               id="itemSearch"
               onChange={ this.handleChange }
+              data-testid="query-input"
             />
           </label>
         </form>
+        <button
+          type="button"
+          onClick={ this.filterItems }
+          data-testid="query-button"
+        >
+          Filtrar
+        </button>
         <div>
           { isInputEmpty
             && (
@@ -50,6 +97,17 @@ export default class Home extends Component {
         <aside>
           <Categories />
         </aside>
+        {
+          itemConteiner.length > 0
+            ? itemConteiner.map((item) => (
+              <div key={ item.title } data-testid="product">
+                <p>{item.title}</p>
+                <img src={ item.thumbnail } alt={ item.title } />
+                <p>{item.price }</p>
+              </div>
+            ))
+            : <p>Nenhum produto foi encontrado</p>
+        }
       </div>
     );
   }
